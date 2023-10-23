3dee8b99bb384cb2465840b517f3d6a4b46a96c4
Brian Foster <brianisbrilliant@users.noreply.github.com>
2023-10-22 22:11:01 -0500

    added FPSController

--- Assets/Scripts/WreckingBall.cs
+++ Assets/Scripts/WreckingBall.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,23 +10,25 @@ public class WreckingBall : MonoBehaviour
     [SerializeField]
     private float launchForce = 30;
     [SerializeField]
-    private float returnIntervalInSeconds = 2;
+    private float returnintervalInSeconds = 2;
     [SerializeField]
     private AnimationCurve curve;
-    
+
 
     private Rigidbody rb;
     private Transform ballStart;
     private bool readyToLaunch = true;
 
-    void Start() {
+    void Start()
+    {
         rb = this.GetComponent<Rigidbody>();
         rb.isKinematic = true;
         ballStart = GameObject.Find("BallStart").transform;
     }
 
     // remove this when attaching to player ship.
-    void Update() {
+    void Update()
+    {
         if(Input.GetKeyDown(KeyCode.Mouse0) && readyToLaunch) {
             Launch();
         }
@@ -41,36 +44,33 @@ public class WreckingBall : MonoBehaviour
         readyToLaunch = false;
         StartCoroutine(Return());
         rb.isKinematic = false;
-        rb.AddForce(ballStart.forward * launchForce, ForceMode.Impulse);
+        rb.AddForce( ballStart.forward * launchForce, ForceMode.Impulse);
     }
 
     private IEnumerator Return() {
         yield return new WaitForSeconds(returnDelay);
-        rb.isKinematic = true;                       // can we set this later in the lerp? No.
+        rb.isKinematic = true; // can we set this later in the lerp? No.
         // lerp back to start position
-        // this.transform.localPosition = new Vector3(0, 0, 0);            // endposition
-        // this.transform.localRotation = Quaternion.identity;
+        //this.transform.localPosition = new Vector3(0, 0, 0); // endposition
+        //this.transform.localRotation = Quaternion.identity;
 
         float counter = 0;
+        // float returnIntervalInSeconds = 5;
         Vector3 startPosition = this.transform.position;
-        // Vector3 endPosition = ballStart.position;
+        //Vector3 endposition = ballStart.position;
 
         Quaternion startRotation = this.transform.rotation;
 
-
-        while(counter < 1) {
-            counter += Time.deltaTime / returnIntervalInSeconds;
+        while (counter < 1) {
+            counter += Time.deltaTime / returnintervalInSeconds;
 
             // lerping position
             this.transform.position = Vector3.Lerp(startPosition, ballStart.position, curve.Evaluate(counter));
-
             // lerping rotation
             this.transform.rotation = Quaternion.Lerp(startRotation, ballStart.rotation, curve.Evaluate(counter));
-
             yield return new WaitForEndOfFrame();
         }
-           
-        readyToLaunch = true;
 
+        readyToLaunch = true;
     }
 }
